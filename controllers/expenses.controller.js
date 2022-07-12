23061e1970494d752dfed1517f7314663ee4b5e4
Luka <lukakavtaradze@gmail.com>
2022-07-12 18:00:18 +0400

    correct validation

--- controllers/expenses.controller.js
+++ controllers/expenses.controller.js
@@ -13,8 +13,11 @@ exports.get = async (req, res) => {
 exports.add = async (req, res) => {
   const { shop } = req.body;
   const { price } = req.body;
-  if (!shop || !price || price <= 0 || isNaN(price)) {
-    return res.status(422).send({ answer: "Shop name or price isn't valid!" });
+  if (!shop) {
+    return res.status(422).send({ answer: "Shop name is not valid!" });
+  }
+  if (price <= 0 || isNaN(price)) {
+    return res.status(422).send({ answer: "Please enter valid number" });
   }
   try {
     return (await expenses.create(req.body)) && (await this.get(req, res));
