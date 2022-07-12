431bce95920884fc4d7b74a06b365b95a1e03475
Luka <lukakavtaradze@gmail.com>
2022-07-12 17:49:12 +0400

    bug fix on feature/add

--- controllers/expenses.controller.js
+++ controllers/expenses.controller.js
@@ -14,14 +14,14 @@ exports.add = async (req, res) => {
   const { shop } = req.body;
   const { price } = req.body;
   if (!shop || !price) {
-    res.status(422).send({ answer: "Shop or price isn't defined!" });
+    return res.status(422).send({ answer: "Shop or price isn't defined!" });
   }
   if (price <= 0 || isNaN(price)) {
-    res.status(422).send({ answer: "Price is not valid" });
+    return res.status(422).send({ answer: "Price is not valid" });
   }
   try {
     return (await expenses.create(req.body)) && (await this.get(req, res));
   } catch (err) {
-    res.status(422).send({ answer: err });
+    return res.status(422).send({ answer: err });
   }
 };
