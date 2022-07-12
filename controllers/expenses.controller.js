f2b9ba184b59574b980d51591befeeba6106d658
Luka <lukakavtaradze@gmail.com>
2022-07-12 14:12:26 +0400

    feature/add commit

--- controllers/expenses.controller.js
+++ controllers/expenses.controller.js
@@ -8,3 +8,19 @@ exports.get = async (req, res) => {
     res.status(422).send({ answer: err });
   }
 };
+
+// add
+exports.add = (req, res) => {
+  if (!req.body["shop"]) {
+    res.status(422);
+    res.send({ answer: "Text is required" });
+  }
+  expenses
+    .create(req.body)
+    .then(async (data) => {
+      res.send(await expenses.findAll());
+    })
+    .catch((err) => {
+      res.status(422).send({ answer: err });
+    });
+};
